929ee72be7f4b1eb3814a60734c20505aafd94f0
Thang <Tcao2315@gmail.com>
2022-05-26 22:20:23 +0700

    commit2

--- app/src/main/java/com/example/onthi/Mahinhchinh.java
+++ app/src/main/java/com/example/onthi/Mahinhchinh.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.List;
+
 public class Mahinhchinh extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.manhinhchinh);
+        List<entity>liss;
     }
 }
